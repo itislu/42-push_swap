60e362cff6a577f548858e446fe9fa93cb9ebd21
Lukas Dullinger <ldulling@c4r2p10.42vienna.com>
2023-11-23 21:45:05 +0100

    fix: Fixed find_pos_sorted stepping beyond the block_size bc it went until the end of the linked list, not until the end of the block

--- src/utils/find_pos_sorted.c
+++ src/utils/find_pos_sorted.c
@@ -14,23 +14,29 @@
 
 void	find_pos_sorted(t_list_d *start, int block_size, t_next get_next)
 {
+	int			i;
+	int			j;
 	t_list_d	*cur;
-	t_list_d	*highest;
+	t_list_d	*lowest;
 
-	while (block_size > 0)
+	i = 0;
+	while (i++ < block_size)
 	{
+		j = 0;
 		cur = start;
 		while (cur->pos_sorted != 0)
-			cur = get_next(cur);
-		highest = cur;
-		cur = get_next(cur);
-		while (cur)
 		{
-			if (cur->pos_sorted == 0 && cur->content >= highest->content)
-				highest = cur;
+			cur = get_next(cur);
+			j++;
+		}
+		lowest = cur;
+		cur = get_next(cur);
+		while (++j < block_size)
+		{
+			if (cur->pos_sorted == 0 && cur->content < lowest->content)
+				lowest = cur;
 			cur = get_next(cur);
 		}
-		highest->pos_sorted = block_size;
-		block_size--;
+		lowest->pos_sorted = i;
 	}
 }
