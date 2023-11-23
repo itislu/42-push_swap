98d10770e1038111ba2ffb345c30f525914d5e6d
Lukas Dullinger <ldulling@c4r2p10.42vienna.com>
2023-11-23 22:25:28 +0100

    refactor: Moved protocol handling from separation function to main sorting handling function per stack end

--- src/algorithm/sort_block_top_b.c
+++ src/algorithm/sort_block_top_b.c
@@ -1,18 +1,23 @@
 #include "push_swap.h"
 
-int	separate_block_top_b(t_heads *heads, int block_size, t_lifo **protocol);
+void	separate_block_top_b(t_heads *heads, int block_size);
 
 bool	sort_block_top_b(t_heads *heads, t_lifo **protocol)
 {
 	find_pos_sorted(heads->top_b, (*protocol)->block_size, get_next);
 	if ((*protocol)->block_size <= 3)
 		sort_rest_top_b(heads, protocol);
-	else if (!separate_block_top_b(heads, (*protocol)->block_size, protocol))
-		return (ft_lstclear_d(&heads->top_a), ft_lstclear_d(&heads->top_b), lifo_lstclear(protocol), false);
+	else
+	{
+		separate_block_top_b(heads, (*protocol)->block_size);
+		lifo_lstclear_n(protocol, 1);
+		if (!add_amounts_to_protocol_top_b(protocol, (*protocol)->block_size))
+			return (ft_lstclear_d(&heads->top_a), ft_lstclear_d(&heads->top_b), lifo_lstclear(protocol), false);
+	}
 	return (true);
 }
 
-int	separate_block_top_b(t_heads *heads, int block_size, t_lifo **protocol)
+void	separate_block_top_b(t_heads *heads, int block_size)
 {
 	int	i;
 
@@ -34,8 +39,4 @@ int	separate_block_top_b(t_heads *heads, int block_size, t_lifo **protocol)
 		}
 		i++;
 	}
-	lifo_lstclear_n(protocol, 1);
-	if (!add_amounts_to_protocol_top_b(protocol, block_size))
-		return (false);
-	return (true);
 }
