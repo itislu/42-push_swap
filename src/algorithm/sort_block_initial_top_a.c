5015b06d5b09bdf8be031328bb196e2bf8ec9226
Lukas Dullinger <ldulling@c1r1p3.42vienna.com>
2023-11-25 09:56:22 +0100

    feat: Added check if initial stack is already sorted -> do nothing

--- src/algorithm/sort_block_initial_top_a.c
+++ src/algorithm/sort_block_initial_top_a.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   algorithm.c                                        :+:      :+:    :+:   */
+/*   sort_block_initial_top_a.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 13:59:15 by ldulling          #+#    #+#             */
-/*   Updated: 2023/11/20 23:58:04 by ldulling         ###   ########.fr       */
+/*   Updated: 2023/11/25 09:54:43 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,13 @@ bool	separate_block_initial_top_a(t_heads *heads, int block_size, t_lifo **proto
 bool	sort_block_initial_top_a(t_heads *heads, t_lifo **protocol)
 {
 	int	block_size;
-	//heads->bottom_a->next = heads->top_a;
 
 	block_size = (*protocol)->block_size;
 	lifo_lstclear_n(protocol, 1);
-	// Split first third of a
+	if (is_n_amount_sorted(heads->top_a, block_size, asc, get_next))
+		return (true);
 	if (!separate_block_initial_top_a(heads, block_size, protocol))
 		return (ft_lstclear_d(&heads->top_a), ft_lstclear_d(&heads->top_b), lifo_lstclear(protocol), false);
-	//heads->bottom_a->next = NULL;
-	// Now collect the groups of 2 or 3
-	// sort_rest_top_a(heads, protocol);
-	// sort_rest_top_b(heads, protocol);
-	// sort_rest_bottom_b(heads, protocol);
 
 	//TODO: Can be made general in all the sort_block functions instead of in each operation.
 	/* Reset pos_sorted in case the largest 2 or 3 inputs were already sorted */
@@ -49,15 +44,12 @@ bool	separate_block_initial_top_a(t_heads *heads, int block_size, t_lifo **proto
 {
 	int	i;
 
-	/* Base case */
 	if(block_size <= 3)	// Would be great to stop earlier, with 4 or 5
 	{
-		//lifo_lstclear_n(protocol, 1);
 		if (!add_single_amount_to_protocol(protocol, block_size, TOP_A))
 			return (false);
 		return (true);
 	}
-
 	find_pos_sorted(heads->top_a, block_size, get_next);
 	i = 0;
 	while (i < block_size)
